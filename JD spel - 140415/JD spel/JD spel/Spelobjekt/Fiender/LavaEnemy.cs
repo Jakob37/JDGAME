68392d37051134c7130575893b99b2ef2736057a
dannew1 <daniel.willforss@skola.ljungby.se>
2014-05-26 19:08:56 +0200

    ?

--- JD spel - 140415/JD spel/JD spel/Spelobjekt/Fiender/LavaEnemy.cs
+++ JD spel - 140415/JD spel/JD spel/Spelobjekt/Fiender/LavaEnemy.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
-namespace JD_spel.Spelobjekt.Fiender
+namespace JD_spel
 {
-    class LavaEnemy
+    class LavaEnemy : Fiende
     {
+        private int skjutTimer;
+        public LavaEnemy(Game1 game, Sprite spriteSheet, Gubbe gubbe, State presentState)
+            : base(game, spriteSheet, gubbe, presentState)
+        {
+            bild = spriteSheet.GetSubSprite(new Rectangle(275, 7, 39, 39));
+            hastighet = 0;
+            movement = FiendeMovement.Static;
+            liv = 1000;
+            skada = 1;
+        }
+        public override void Uppdatera(GameTime gameTime)
+        {
+            base.Uppdatera(gameTime);
+            Skjuta(gameTime);
+        }
+        private void Skjuta(GameTime gameTime)
+        {
+            skjutTimer += gameTime.ElapsedGameTime.Milliseconds;
+            if (skjutTimer >= 1500)
+            {
+                
+                skjutTimer = 0;
+            }
+        }
     }
 }
